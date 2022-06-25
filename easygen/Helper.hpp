e9a3eabe70029a91c5e283b4261859ff2cfe4e67
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2022-06-25 16:34:35 +0300

    io helper

---
+++ easygen/Helper.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <sstream>
+#include <boost/filesystem.hpp>
+#include <boost/dll.hpp>
+#include <boost/filesystem/string_file.hpp>
+
+namespace easygen
+{
+	namespace io
+	{
+		inline boost::filesystem::path get_current_path()
+		{
+			return boost::dll::program_location().parent_path();
+		}
+
+		inline boost::filesystem::path get_path_to_temp_dir()
+		{
+			return get_current_path() / "tmp";
+		}
+
+		inline boost::filesystem::path get_path_to_template_dir()
+		{
+			return get_current_path() / "templates";
+		}
+
+	}
+}
