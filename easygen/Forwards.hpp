c46b77ebb46feca685f70d9d3790877a79dee3ac
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2022-06-25 16:33:49 +0300

    forwards

---
+++ easygen/Forwards.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+namespace easygen
+{
+	/*fwd decl*/
+	struct DataModelDescription;
+
+	using ListDataModels = std::vector<DataModelDescription>;
+	using ErrorMsg = std::string;
+
+
+	using DefaultValue = std::string;
+	using PlatformType = std::string;
+	using DataTypeMapping = std::map<std::string, std::tuple<PlatformType, DefaultValue>>;
+}
