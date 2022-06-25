0111243136bb100019f99a8a54357c1bbff7e1ee
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2022-06-25 16:34:59 +0300

    Create MsgPackCppGenerator.hpp

---
+++ easygen/generators/MsgPackCppGenerator.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "AbstractGenerator.hpp"
+#include "DataTypeBasic.hpp"
+
+namespace easygen
+{
+	class MsgPackCppGenerator : public easygen::AbstractGenerator
+	{
+	public:
+		MsgPackCppGenerator(const std::vector<easygen::DataModelDescription>& models)
+			: AbstractGenerator(easygen::CppBaseTypes(), models)
+		{
+		}
+
+		std::vector<std::string> GenerateData(const std::string& output_dir) override;
+	};
+}
