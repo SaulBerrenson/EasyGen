098e1abfc5eece770d44b585ad1467cdd55aae90
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2022-06-25 16:34:09 +0300

    data model properties

---
+++ easygen/PropertyDescription.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <string>
+
+
+namespace easygen
+{
+	class PropertyDescription
+	{
+	public:
+		std::string m_name;
+		std::string m_type;
+		std::string m_description;
+		bool is_last = false;
+	};
+}
