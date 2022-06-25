6fb991f33b2b81a7c60c951797efb9890d05a9b6
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2022-06-25 16:34:00 +0300

    naming for data model

---
+++ easygen/Labels.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+namespace easygen
+{
+	namespace labels
+	{
+		constexpr const char* class_name = "class_name";
+		constexpr const char* class_name_lower = "class_name_lower";
+		constexpr const char* class_name_upper = "class_name_upper";
+		constexpr const char* namespace_name = "namespace_name";
+
+		constexpr const char* property_list = "properties";
+		constexpr const char* property_name = "property_name";
+		constexpr const char* property_last = "last";
+		constexpr const char* property_type = "property_type";
+		constexpr const char* property_default_value = "default_value";
+		constexpr const char* property_description = "property_description";
+
+	}
+}
